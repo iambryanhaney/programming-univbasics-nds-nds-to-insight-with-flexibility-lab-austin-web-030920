8dc442f4432f880a9c1c07863b08efe8bb883c05
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-02 02:34:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,7 +70,6 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-  pp collection
   i = 0
   studio_totals = {}
   while i < collection.length do
